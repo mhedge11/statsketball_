2d7b56e8818c189f8c88c0d29d199b95a2d8be79
unknown <wu1272@purdue.edu>
2020-10-17 12:10:04 -0400

    reads player table with JSON response

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
+//grab database info from env config
+require('dotenv').config()
+
+//dependencies for express, body-parser and database
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
 
+//test / for Hello World
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('App listening on port 3000'));
+//going to port/3306 should show query
+app.route('/player/')
+  .get(function(req, res, next) {
+    connection.query(
+      "select * from Player",
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
+
+app.set('port', process.env.PORT || 3306);
+app.listen(3306, () => console.log('App listening on port 3306'));
--- app/database.js
+++ app/database.js
@@ -5,7 +5,7 @@ let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS
+    password: process.env.DB_PASS,
   });
   
 //attempt connection with GCP MySQL database
